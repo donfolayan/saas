24051ce1366053df27f72e0ee1573d7da4f99800
Donald Folayan <donfolayan@gmail.com>
2025-03-17 22:13:20 +0100

    Setting up secrets

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -19,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#1&b7r!p07v!hz44zx!))(pj%*e&f1(%s9an^zau@9@or1e03f'
+SECRET_KEY = config('DJANGO_SECRET_KEY)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = config('DJANGO_DEBUG', cast=bool)
 
 ALLOWED_HOSTS = [
     '.railway.app',

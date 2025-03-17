4a8f3ca41a78a64019199e0488decd5eae1c7b9f
Donald Folayan <donfolayan@gmail.com>
2025-03-17 21:14:03 +0100

    init

---
+++ src/saas/views.py
@@ -0,0 +1,41 @@
+import pathlib
+from django.http import HttpResponse
+from django.shortcuts import render
+from visits.models import PageVisits
+
+
+def home_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent_visit = (page_qs.count()/qs.count())*100
+    except ZeroDivisionError:
+        percent_visit = 0
+    my_title = "Homepage"
+    my_context = {
+        'page_title' : my_title,
+        'total_visit_count' : qs.count(),
+        'page_visit_count' : page_qs.count(),
+        'percent_visit': percent_visit,
+    }
+    PageVisits.objects.create(path=request.path)
+    html_template  = "home.html"
+    return render(request, html_template, my_context)
+
+def about_view(request, *args, **kwargs):
+    qs = PageVisits.objects.all()
+    page_qs = PageVisits.objects.filter(path=request.path)
+    try:
+        percent_visit = (page_qs.count()/qs.count())*100
+    except ZeroDivisionError:
+        percent_visit = 0
+    my_title = "Homepage"
+    my_context = {
+        'page_title' : my_title,
+        'total_visit_count' : qs.count(),
+        'page_visit_count' : page_qs.count(),
+        'percent_visit': percent_visit,
+    }
+    PageVisits.objects.create(path=request.path)
+    html_template  = "home.html"
+    return render(request, html_template, my_context)

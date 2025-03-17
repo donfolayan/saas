af9ff348d3981555eddf2ed5fab107eaf9baca5b
Donald Folayan <donfolayan@gmail.com>
2025-03-17 23:56:03 +0100

    Prepare static files

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -133,6 +133,17 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_BASE_DIR = BASE_DIR / 'staticfiles'
+STATICFILES_VENDOR_DIR = BASE_DIR / 'staticfiles/vendor'
+
+STATICFILES_DIRS = [
+    STATICFILES_BASE_DIR,
+]
+
+#cdn
+STATIC_ROOT = BASE_DIR / 'local_cdn'
+if not DEBUG:
+    STATIC_ROOT = BASE_DIR / 'prod_cdn'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field

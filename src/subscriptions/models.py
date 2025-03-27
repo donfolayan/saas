5dbb1eb0db5948650c2a2eb81a278c3ae9bddcf1
Donald Folayan <donfolayan@gmail.com>
2025-03-27 16:02:50 +0100

    feat: added subscription model to track groups and perms

--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,12 +1,25 @@
 from django.db import models
+from django.contrib.auth.models import Group, Permission
 
 # Create your models here.
-class Subscription(models.Model):
-    name = models.CharField(max_length=120)
 
-    class Meta:
-        permissions = [
+SUBSCRIPTION_PERMISSIONS = [
             ('advanced', 'Advanced Perm'),
             ('pro', 'Pro Perm'),
             ('basic', 'Basic Perm'),
+            ('basic ai', 'Basic AI Perm'),
         ]
+
+class Subscription(models.Model):
+    name = models.CharField(max_length=120)
+    groups = models.ManyToManyField(Group)
+    permissions = models.ManyToManyField(
+        Permission, 
+        limit_choices_to={
+        'content_type__app_label': 'subscriptions',
+        'codename__in': [x[0] for x in SUBSCRIPTION_PERMISSIONS],
+        }
+    )
+
+    class Meta:
+        permissions = SUBSCRIPTION_PERMISSIONS

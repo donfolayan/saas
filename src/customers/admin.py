9a75455ba2a2b896e8b063c54e5559173c1d1a26
Donald Folayan <donfolayan@gmail.com>
2025-04-24 17:15:40 +0100

    Debug: Errors with users; Added tabs to customer field for easy debug

--- src/customers/admin.py
+++ src/customers/admin.py
@@ -2,4 +2,10 @@ from django.contrib import admin
 from .models import Customer
 # Register your models here.
 
-admin.site.register(Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ('user', 'stripe_id', 'init_email', 'init_email_confirmed')  # Fields to display in the list view
+    search_fields = ('user__username', 'stripe_id', 'init_email')  # Fields to search by
+    list_filter = ('init_email_confirmed',)  # Add filters for boolean fields
+
+# Register the model with the custom admin class
+admin.site.register(Customer, CustomerAdmin)

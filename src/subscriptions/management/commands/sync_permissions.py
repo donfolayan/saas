7e75d9b8db4b6364635f47afde97a3fe10c2c1c8
Donald Folayan <donfolayan@gmail.com>
2025-04-26 09:49:48 +0100

    feat: Refactored management commands for readability; Modularize syncing user sub and syncing permissions

--- src/subscriptions/management/commands/sync_permissions.py
+++ src/subscriptions/management/commands/sync_permissions.py
@@ -1,12 +1,8 @@
 from typing import Any
 from django.core.management.base import BaseCommand
-from subscriptions.models import Subscription
+from subscriptions import utils as subs_utils
 
 class Command(BaseCommand):
 
     def handle(self, *args:Any, **kwargs:Any):
-        qs = Subscription.objects.filter(active=True)
-        for obj in qs:
-            sub_perms = obj.permissions.all()
-            for group in obj.groups.all():
-                group.permissions.set(sub_perms)
+        subs_utils.sync_sub_group_permissions()

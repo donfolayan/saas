70d061de113816c490c35b9154dd840ab31ea4a8
Donald Folayan <donfolayan@gmail.com>
2025-04-27 14:58:36 +0100

    feat: Add custom management command to filter model with Datetime objects

--- src/subscriptions/management/commands/sync_user_subs.py
+++ src/subscriptions/management/commands/sync_user_subs.py
@@ -7,15 +7,29 @@ from subscriptions import utils as subs_utils
 class Command(BaseCommand):
 
     def add_arguments(self, parser):
+        parser.add_argument('--day-start', default=0, type=int)
+        parser.add_argument('--day-end', default=0, type=int)
+        parser.add_argument('--days-left', default=0, type=int)
+        parser.add_argument('--days-ago', default=0, type=int)
         parser.add_argument('--clear-dangling', action='store_true', default=False)
 
     def handle(self, *args:Any, **options:Any):
+        days_left = options.get('days_left')
+        days_ago = options.get('days_ago')
+        day_start = options.get('day_start')
+        day_end = options.get('day_end')
         clear_dangling = options.get('clear_dangling')
         if clear_dangling:
             print('Clearing dangling subscriptions')
             subs_utils.clear_dangling_subs()
         else:
             print('Sync active subscriptions')  
-            done = subs_utils.refresh_active_user_subscriptions(active_only=True, verbose=True)
+            done = subs_utils.refresh_active_user_subscriptions(
+                active_only=True, 
+                days_left=days_left, 
+                days_ago=days_ago, 
+                day_start=day_start,
+                day_end=day_end,
+                verbose=True)
             print('Finished syncing active subscriptions') if done else print('Failed syncing active subscriptions')
         
--- src/subscriptions/models.py
+++ src/subscriptions/models.py
@@ -1,3 +1,4 @@
+import datetime
 import helpers.billing
 from django.db import models
 from django.conf import settings
@@ -6,6 +7,7 @@ from django.db.models.signals import post_save
 from django.conf import settings
 from django.urls import reverse
 from django.db.models import Q
+from django.utils import timezone
 
 User = settings.AUTH_USER_MODEL
 ALLOW_CUSTOM_GROUPS = True
@@ -155,9 +157,32 @@ class SubscriptionStatus(models.TextChoices):
         PAUSED = 'paused', 'Paused'
 
 class UserSubscriptionQuerySet(models.QuerySet):
+    def by_range(self, day_start=7, day_end=120):
+        now = timezone.now()
+        days_start_from_now = now + datetime.timedelta(days=day_start)
+        days_end_from_now = now + datetime.timedelta(days=day_end)
+        range_start = days_start_from_now .replace(hour=0, minute=0, second=0, microsecond=0)
+        range_end = days_end_from_now .replace(hour=23, minute=59, second=59, microsecond=999999)
+        return self.filter(current_period_end__gte=range_start, current_period_end__lte=range_end)
+    
+    def by_days_left(self, days_left=7):
+        now = timezone.now()
+        in_n_days = now + datetime.timedelta(days=days_left)
+        day_start = in_n_days .replace(hour=0, minute=0, second=0, microsecond=0)
+        day_end = in_n_days .replace(hour=23, minute=59, second=59, microsecond=999999)
+        return self.filter(current_period_end__gte=day_start, current_period_end__lte=day_end)
+    
+    def by_days_ago(self, days_ago=3):
+        now = timezone.now()
+        in_n_days = now - datetime.timedelta(days=days_left)
+        day_start = in_n_days .replace(hour=0, minute=0, second=0, microsecond=0)
+        day_end = in_n_days .replace(hour=23, minute=59, second=59, microsecond=999999)
+        return self.filter(current_period_end__gte=day_start, current_period_end__lte=day_end)
+
+
     def by_active_trialing(self):
-            active_qs_loookup = (Q(status = SubscriptionStatus.ACTIVE) | Q(status = SubscriptionStatus.TRIALING))
-            return self.filter(active_qs_loookup)
+        active_qs_loookup = (Q(status = SubscriptionStatus.ACTIVE) | Q(status = SubscriptionStatus.TRIALING))
+        return self.filter(active_qs_loookup)
 
     def by_user_ids(self, user_ids=None):
         qs = self

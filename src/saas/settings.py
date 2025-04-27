6b4a1e20a5a366c361d8a778420d17d779a6104c
Donald Folayan <donfolayan@gmail.com>
2025-04-27 20:58:04 +0100

    fix: changed conn max age default to 0 so test connections can be dropped fast

--- src/saas/settings.py
+++ src/saas/settings.py
@@ -132,7 +132,7 @@ DATABASES = {
     }
 }
 
-CONN_MAX_AGE = config('CONN_MAX_AGE', default=30 ,cast=int)
+CONN_MAX_AGE = config('CONN_MAX_AGE', default=0 ,cast=int)
 DATABASE_URL = config('DATABASE_URL', default=None)
 
 if DATABASE_URL is not None:
